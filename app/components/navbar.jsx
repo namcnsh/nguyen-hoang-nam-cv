e59906c5d2ff29180c1a76c88256448ccec08fcd
namcnsh <namcnsh@gmail.com>
2026-06-14 22:42:06 +0700

    style: increase navbar logo size

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
           <Link
             href="/"
             className="text-[#16f2b3] text-2xl sm:text-3xl font-bold">
-            <img src="/logoseilybvn.png" alt="Logo" className="h-10 sm:h-12" />
+            <img src="/logoseilybvn.png" alt="Logo" className="h-16 sm:h-20" />
           </Link>
         </div>
 
